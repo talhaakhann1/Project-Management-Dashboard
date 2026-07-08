8542994c1be739b8a52a27e7debce855409c0d28
Talha-ty <tk2020170@gmail.com>
2026-07-08 13:27:24 +0500

    Define global constants

---
+++ backend/src/constant.ts
@@ -0,0 +1,17 @@
+export const DB_NAME="project-management-app"
+
+
+export const UserRolesEnum = {
+  ADMIN: "ADMIN",
+  USER: "USER",
+};
+
+export const AvailableUserRoles = Object.values(UserRolesEnum);
+
+export const UserLoginType = {
+  GOOGLE: "GOOGLE",
+  GITHUB: "GITHUB",
+  EMAIL_PASSWORD: "EMAIL_PASSWORD",
+};
+
+export const AvailableSocialLogins = Object.values(UserLoginType);
