fb7f145b85d377833d9b75671598ded1a454447a
talhaakhann1 <talhakhan3620@gmail.com>
2026-09-03 02:02:24 +0500

    refac: user interface

--- frontend/src/types/enums/user.enum.ts
+++ frontend/src/types/enums/user.enum.ts
@@ -1,20 +1,22 @@
 export interface availableMembers {
-    id: string,
-    fullName: string
+  id: string;
+  fullName: string;
 }
 export interface availableAssignees {
-    id: string,
-    fullName: string
+  id: string;
+  fullName: string;
 }
 
 export interface User {
+  id: string;
   fullName: string;
   email: string;
-  password:string;
+  password: string;
   avatar: { url: string; localPath: string };
-  refreshToken:string;
-  accessToken:string;
-  role:string;
-  isActive:boolean;
-
+  refreshToken: string;
+  accessToken: string;
+  role: string;
+  isActive: boolean;
+  createdAt: Date;
+  updatedAt: Date;
 }
