de6a8b3256a8e4f3ac2505b4a27ee29a6627ae6b
talhaakhann1 <talhakhan3620@gmail.com>
2026-09-03 02:07:13 +0500

    refac: add new routes

--- backend/src/routes/project.routes.ts
+++ backend/src/routes/project.routes.ts
@@ -6,7 +6,7 @@ import {
   getAllProjects,
   getProjectById,
   getAllAvailableProjects,
-  changeProjectStatus
+  changeProjectStatus,
 } from "../controllers/project.controller.js";
 import { createProjectSchema, updateProjectSchema, updateProjectStatus } from "../Schemas/project.schema.js";
 import { validate } from "../Schemas/validate.js";
@@ -21,6 +21,7 @@ router.route('/:projectId').delete(verifyJwt,deleteProjectById)
 router.route('/').get(verifyJwt,getAllProjects)
 router.route('/all').get(verifyJwt,getAllAvailableProjects)
 router.route('/:projectId').get(verifyJwt,getProjectById)
+
 
 
 export default router;
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -12,6 +12,7 @@ import { signInSchema } from "../Schemas/signInSchema.js";
 import { validate } from "../Schemas/validate.js";
 import { verifyJwt } from "../middleware/auth.middleware.js";
 import { upload } from "../middleware/multer.middleware.js";
+import { getTeamMembers } from "../controllers/user.controller.js";
 
 const router=Router()
 
@@ -22,5 +23,6 @@ router.route('/refresh-token').get(refreshAccessToken)
 router.route('/change-avatar').post(verifyJwt,upload.single("avatar"),changeUserAvatar)
 router.route('/get-user').get(verifyJwt,getCurrentUser)
 router.route('/get-users').get(verifyJwt,getAvailableUsers)
+router.route('/team-members').get(verifyJwt,getTeamMembers)
 
 export default router
