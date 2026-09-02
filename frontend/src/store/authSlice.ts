4d9e1c6141b4671349e00f7999d9516383970610
talhaakhann1 <talhakhan3620@gmail.com>
2026-09-03 02:03:26 +0500

    refac: remove unwanted lines of code

--- frontend/src/store/authSlice.ts
+++ frontend/src/store/authSlice.ts
@@ -1,17 +1,15 @@
+import { User } from "@/types/enums/user.enum";
 import { createSlice } from "@reduxjs/toolkit";
-import { User } from "@/Schemas/user.schema";
 import { PayloadAction } from "@reduxjs/toolkit";
 
 type AuthSlice = {
   isLoggedIn: boolean;
   user: User | null;
-  loading:boolean
 };
 
 const initialState: AuthSlice = {
   isLoggedIn: false,
   user: null,
-  loading:true
 };
 
 export const authSlice = createSlice({
@@ -26,15 +24,12 @@ export const authSlice = createSlice({
       state.isLoggedIn = false;
       state.user=null;
     },
-    setLoading(state,action){
-      state.loading=action.payload
-    },
     updateUserDetails(state,action:PayloadAction<User>){
       state.user=action.payload;
     }
   },
 });
 
-export const { login, logout,updateUserDetails,setLoading } = authSlice.actions;
+export const { login, logout,updateUserDetails } = authSlice.actions;
 
 export default authSlice.reducer;
